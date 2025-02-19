f24e7fe0ba3b0d215a4136977303cedbc5ceeff7
wu-yiqiang <wu_yiqiang@outlook.com>
2025-02-20 01:16:36 +0800

    新增用户中间件

--- user/middleware.py
+++ user/middleware.py
@@ -1,9 +1,22 @@
+import jwt
+from django.http import JsonResponse, HttpResponse
 from django.utils.deprecation import MiddlewareMixin
+from rest_framework.settings import api_settings
 
 
 class JwtAuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        white_list= ['/user/login']
+        white_list = ['/user/login']
         path = request.path
-        if path not in white_list and path.startswith('/midia'):
-            pass
+        if path not in white_list and not path.startswith('/midia'):
+            try:
+                token = request.META.get('HTTP_AUTHORIZATION')
+                jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
+                jwt_decode_handler(token)
+            except jwt.ExpiredSignatureError as e:
+                return HttpResponse({'data': {}, 'msg': "token expired"})
+            except jwt.InvalidTokenError as e:
+                return HttpResponse({'code': 500, 'msg': 'token验证失败', 'data': {}})
+            except Exception as e:
+                return HttpResponse({'code': 500, 'msg': 'token验证异常', 'data': {}})
+        pass
--- user/views.py
+++ user/views.py
@@ -1,10 +1,13 @@
 import json
+
+from django.http import JsonResponse
 from django.shortcuts import render, HttpResponse
 from django.views import View
+from pycparser.ply.yacc import token
 from rest_framework_jwt.settings import api_settings
 from service_error.common import COMMON_RERROR
 from service_error.user import USER_RERROR
-from user.models import SysUser
+from user.models import SysUser, SysUserSerializer
 
 
 class LoginView(View):
@@ -23,11 +26,11 @@ class LoginView(View):
             jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
             payload = jwt_payload_handler(user)
             token = jwt_encode_handler(payload)
-            return HttpResponse(token)
         except Exception as e:
             print(e)
             return HttpResponse(USER_RERROR.USER_OR_PASSWORD_ERROR['msg'])
-        return HttpResponse("post")
+        return JsonResponse(
+            {"code": 200, "data": {"token": token, "data": SysUserSerializer(user).data, "msg": 'success'}})
 
 
 class UserView(View):
