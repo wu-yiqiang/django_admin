48380238eaf16fd5dd02d7ac84907b6a257e3568
wu-yiqiang <wu_yiqiang@outlook.com>
2025-03-02 11:31:42 +0800

    修改部署脚本

--- reboot.sh
+++ reboot.sh
@@ -1,6 +1,6 @@
 #!/user/bin/env bash
 
-echo -e "\033[34m---uwsgi deploy start---\033[0m"
+echo -e "\024[34m---uwsgi deploy start---\024[0m"
 
 ps -ef | grep ./conf/uwsgi.ini | grep -v grep
 
@@ -8,15 +8,15 @@ sleep 0.5
 
 echo -e "\n--- close ---"
 
-ps -ef | grep./conf/uwsgi.ini | grep -v grep | awk '{print $2}' | xarg kill -9
+ps -ef | grep ./conf/uwsgi.ini | grep -v grep | awk '{print $2}' | xarg kill -9
 
 sleep 0.5
 
 echo -e "\n--- check if the kill action is correct ---"
 
-./venv/bin/uwsgi --ini ./conf/uwsgi.ini & >/dev/null
+./venv/bin/uwsgi --ini ./conf/uwsgi.ini & > /dev/null
 
-echo -e "\033[34m---uwsgi started---\033[0m"
+echo -e "\024[34m---uwsgi started---\024[0m"
 
 sleep 1
 
--- stop.sh
+++ stop.sh
@@ -1,12 +1,12 @@
 #!/user/bin/env bash
 
-echo "\024[34m---uwsgi deploy start---\024[0m"
+echo "\024[34m--- start close ---\024[0m"
 
 ps -ef | grep ./conf/uwsgi.ini | grep -v grep
 
 sleep 0.5
 
-echo -e "\n--- close ---"
+echo -e "\n--- closed ---"
 
 ps -ef | grep ./conf/uwsgi.ini | grep -v grep | awk '{print $2}' | xargs kill -9
 
