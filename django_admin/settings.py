78f3a65cb8da33e36b2b69daea26d859de589b20
wu-yiqiang <wu_yiqiang@outlook.com>
2025-06-29 21:19:49 +0800

    212

--- django_admin/settings.py
+++ django_admin/settings.py
@@ -203,32 +203,32 @@ LOGGING = {
     }
 }
 
-DEBUG = True
-PROTOCOL = 'http'
-IP = '192.168.1.222'
-PORT = '8000'
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_admin',
-        "USER": 'root',
-        "PASSWORD": 'root@root',
-        "HOST": '127.0.0.1',
-        "PORT": '3306',
-    }
-}
-
-# DEBUG = False
-# PROTOCOL = 'https'
-# IP = '185.198.166.245'
+# DEBUG = True
+# PROTOCOL = 'http'
+# IP = '192.168.1.222'
 # PORT = '8000'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
 #         'NAME': 'django_admin',
-#         "USER": 'mydjango',
-#         "PASSWORD": 'mydjango@mydjango',
+#         "USER": 'root',
+#         "PASSWORD": 'root@root',
 #         "HOST": '127.0.0.1',
 #         "PORT": '3306',
 #     }
 # }
+
+DEBUG = False
+PROTOCOL = 'https'
+IP = '185.198.166.245'
+PORT = '8000'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_admin',
+        "USER": 'mydjango',
+        "PASSWORD": 'mydjango@mydjango',
+        "HOST": '127.0.0.1',
+        "PORT": '3306',
+    }
+}
