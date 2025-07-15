b50ce11bd3e1cc9a4d594899219080bf14a2e0f9
wu-yiqiang <wu_yiqiang@outlook.com>
2025-07-16 01:13:58 +0800

    删除冗余数据表

--- common/db.py
+++ common/db.py
@@ -4,8 +4,8 @@ from django.db import models
 class BaseModel(models.Model):
     is_deleted = models.BooleanField(null=False, default=False, verbose_name='删除标记')
     remark = models.CharField(max_length=2500, null=True, verbose_name="备注")
-    create_time = models.DateField(null=True, verbose_name="创建时间")
     update_time = models.DateField(null=True, auto_now=True, verbose_name="更新时间")
+    create_time = models.DateField(null=True, verbose_name="创建时间")
 
     class Meta:
         abstract = True
